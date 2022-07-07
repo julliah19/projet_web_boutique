f7fd11cfe9b2549224d056d660750b4b6d255850
Senhao JIANG <Senhao.Jiang@ibm.com>
2022-07-07 11:13:06 +0200

    Display cart

--- projet/src/component/Cart.js
+++ projet/src/component/Cart.js
@@ -8,24 +8,20 @@ const renderCartProduct = (loading) => {
 
     const data = localStorage.getItem('userData');
     const parse = JSON.parse(data);
-    console.log(parse.cart);
     
-    const Panier = () => {
-        console.log("YEAH");
-        {parse.cart.map((p) => {
-            return(<ClickableTile onClick={() => { setSelectedProduct(p.idProduit); setOpen(true); }} style={{ textAlign: "left", lineHeight: "24px", marginRight: "16px" }}>
+
+    return(<div style={{ display: "flex" }}>{parse.cart.map((p) => {
+
+        return (<ClickableTile style={{ textAlign: "left", lineHeight: "24px", marginRight: "16px" }}>
             {p.img != "" ? <img src={require("../../assets/img/produits/" + p.img)} style={{ maxWidth: "250px", minWidth: "250px", minHeight: "40vh", maxHeight: "40vh", display: "flex", justifyContent: "center", margin: "auto" }} /> : <img src="../../assets/img/produits/no-image-found.jpg" />} <br></br>
             {"Nom du produit : " + p.nomProduit} <br></br>
             {"Description : " + p.description}  <br></br>
             {"Prix : " + p.prix + " €"}  <br></br>
             {"Disponibilité : "} {p.dispo == 1 ? "Oui" : "Non"} <br></br>
             {"Stock : " + p.stock + " unités"}  <br></br>
-    
-        </ClickableTile>);
-        })}
-    }
 
-    return(<div>{Panier()}</div>)
+        </ClickableTile>)
+    })}</div>)
 }
 
 
